bd2962e77606f9b85bc5a22e682c3d8fd3c49df2
LiZhongxin <1254429011@qq.com>
2018-07-06 10:45:31 +0800

    Initial commit

---
+++ src/main/java/com/example/springcloudzuul/SpringcloudZuulApplication.java
@@ -0,0 +1,17 @@
+package com.example.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
+public class SpringcloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudZuulApplication.class, args);
+    }
+}
